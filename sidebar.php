a83cb6a4a047f0deefcbc02affd7d375956d0ef4
Niklas Wichterich <wichterichniklas@googlemail.com>
2020-12-04 16:38:53 +0100

    Sidebar

---
+++ sidebar.php
@@ -0,0 +1,30 @@
+<?php
+ '<div class="row">
+    <div class="col-md-2">
+
+        <ul class="nav flex-column">
+            <span class="border">
+                <a class="nav-link" href="LogIn.html">LogIn</a>
+            </span>
+            <span class="border">
+                <a class="nav-link" href="Projekte.html">Projekte</a>
+            </span>
+            <span class="border">
+                <a class="nav-link" href="index.html">Aktuelles Projekt</a>
+            </span>
+
+
+            <span class="border" style="width: 85%;margin-left:auto">
+                <a class="nav-link" href="Reiter.html">Reiter</a>
+        </span>
+
+            <span class="border"style="width: 85%;margin-left:auto">
+                <a class="nav-link" href="Aufgaben.html">Aufgaben</a>
+            </span>
+            <span class="border"style="width: 85%;margin-left:auto">
+                <a class="nav-link" href="Personen.html">Personen</a>
+            </span>
+        </ul>
+    </div>
+</div>';
+?>
